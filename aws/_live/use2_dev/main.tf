01bb282ef9cb10988b20f8c39e164e0956f3a860
Steven Doss <81587256+sdossoak9@users.noreply.github.com>
2022-10-18 14:58:02 -0500

    Modifying example

--- aws/_live/use2_dev/main.tf
+++ aws/_live/use2_dev/main.tf
@@ -198,12 +198,6 @@ module "Oak9CodeArtifactDomain" {
 }
 
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
 
   attribute {
     name = "UserId"
@@ -222,15 +216,11 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
 
   ttl {
     attribute_name = "TimeToExist"
-    enabled        = false
   }
 
   global_secondary_index {
     name               = "GameTitleIndex"
     hash_key           = "GameTitle"
-    range_key          = "TopScore"
-    write_capacity     = 10
-    read_capacity      = 10
     projection_type    = "INCLUDE"
     non_key_attributes = ["UserId"]
   }
